79aac964a5edcc17ac53707732e52b39ff7a219a
Munsif <mh742@njit.edu>
2025-10-20 19:50:31 -0400

    prior save

--- Day 3/If Else/task.py
+++ Day 3/If Else/task.py
@@ -1,2 +1,9 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+if height >= 120:
+    print("Lets ride!")
+else:
+    print("You're a midget, sorry no midgets. SUCKAAA!")
+
+#Comparison Operators be like -
+# < less than, > greater than, >= greater than or equal to, <= less than or equal to, == precisely
