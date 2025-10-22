f1890aed286991179ce3afa50c73e2f97fac9b5d
Munsif <mh742@njit.edu>
2025-10-21 22:49:28 -0400

    prior save

--- Day 3/Logical Operators/task.py
+++ Day 3/Logical Operators/task.py
@@ -11,15 +11,18 @@ if height >= 120:
     elif age <= 18:
         bill = 7
         print("Youth tickets are $7.")
+    elif age >= 45 and age <= 55:
+        bill = 0
+        print("Everything will be okay. Enjoy the free ride, on us!")
     else:
         bill = 12
         print("Adult tickets are $12.")
-
-    wants_photo = input("Do you want a photo taken? Y or N. ")
-    if wants_photo == "Y":
+wants_photo = input("Do you want a photo taken? Y or N. ")
+if wants_photo == "Y":
         bill += 3
-
-    print(f"Your final bill is ${bill}")
-
+        print(f"Your final bill is ${bill}")
+elif wants_photo == "N":
+        print(f"Your final bill is ${bill}")
 else:
-    print("Sorry, you have to grow taller before you can ride.")
+        print("Sorry, you have to grow taller before you can ride.")
+
