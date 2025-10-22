b203cdbc24e0674ea5cf82c16d509a6f0b633167
Munsif <mh742@njit.edu>
2025-10-21 21:37:02 -0400

    prior save

--- Day 3/Multiple Ifs/task.py
+++ Day 3/Multiple Ifs/task.py
@@ -1,14 +1,25 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height >= 120:
     print("You can ride the rollercoaster")
     age = int(input("What is your age? "))
     if age <= 12:
-        print("Please pay $5.")
+        bill = 5
+        print("Child tickets are $5.")
     elif age <= 18:
-        print("Please pay $7.")
+        bill = 7
+        print("Teen tickets are $7.")
     else:
-        print("Please pay $12.")
+        print("Adult tickets are $12.")
+        bill = 12
+    photo = input("Do you want a photo taken? Type y for Yes, n for No\n")
+    if photo == "y":
+        bill += 3
+    discount = input("Do you have your student discount? Type y for Yes, n for No\n")
+    if discount == "y":
+        bill = bill - (bill*0.2)
+    print(f"Your final bill is ${bill}")
 else:
     print("Sorry you have to grow taller before you can ride.")
