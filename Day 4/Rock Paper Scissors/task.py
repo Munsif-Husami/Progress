8c2fd4325b70f1aba096f65c9799e5070ae109d5
Munsif <mh742@njit.edu>
2025-10-28 19:25:03 -0400

    prior save

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -24,3 +24,37 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+import random
+print("Welcome to Rock, Paper, Scissors!")
+yourmove = input("What do you choose?\nType 0 for rock, 1 for paper, 2 for scissors.\n")
+comp_choices = ["0","1","2"]
+if yourmove == "0":
+    print(rock)
+elif yourmove == "1":
+    print(paper)
+elif yourmove == "2":
+    print(scissors)
+else:
+    print("Invalid Response")
+    pc_move = random.choice(comp_choices)
+    if pc_move == "0":
+        print(rock)
+    elif pc_move == "1":
+        print(paper)
+    elif pc_move == "2":
+        print(scissors)
+    if yourmove == pc_move:
+        print("Its a draw")
+    elif yourmove == "0" and pc_move == comp_choices[1]:
+        print("You lose")
+    elif yourmove == "0" and pc_move == comp_choices[2]:
+        print("You win")
+    elif yourmove == "1" and pc_move == comp_choices[0]:
+        print("You win")
+    elif yourmove == "1" and pc_move == comp_choices[2]:
+        print("You lose")
+    elif yourmove == "2" and pc_move == comp_choices[0]:
+        print("You lose")
+    elif yourmove == "2" and pc_move == comp_choices[1]:
+        print("You win")
