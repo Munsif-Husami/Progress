694e06b00d6c0cd048d78f578998739e5e723e76
MHusami <mh742@njit.edu>
2025-10-31 15:46:52 -0400

    prior save

--- Day 5/For Loops/task.py
+++ Day 5/For Loops/task.py
@@ -1,1 +1,6 @@
-fruits = ["Apple", "Peach", "Pear"]
+fruits = ["Apple", "Peach", "Pear","Pumpkin","Watermelon","Banana"]
+for each in fruits:
+    if each[0] != "A":
+        print(each)
+    else:
+        print("fuck fruits that start with A")
