7ac3c8259efa5436787150b32a9f574a5720cf37
Munsif <mh742@njit.edu>
2025-11-02 19:51:05 -0500

    Created FizzBuzz!

--- Day 5/For Loops with Range/task.py
+++ Day 5/For Loops with Range/task.py
@@ -4,7 +4,17 @@
 #    print(number)
 
 sum_el = 0
+#
+# for number in range(1,101):
+#     sum_el += number
+# print(sum_el)
 
 for number in range(1,101):
-    sum_el += number
-print(sum_el)
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
