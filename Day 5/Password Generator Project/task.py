fec153ca38527a8778b12b1dc5ccb44f6cb75619
Munsif <mh742@njit.edu>
2025-11-06 20:01:59 -0500

    Solved with some help. Needs practice.

--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -9,19 +9,59 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #Easy Level
 import random
+pw_list = ""
 
-#Still struggling - will take another crack later
+for char in range(0, nr_letters):
+    pw_list += random.choice(letters)
 
-pw_list = []
-char = []
-for char in letters:
-    if char > pw_list:
-        pw_list = char
-for sym in symbols:
-    if sym > 0:
-        pw_list.append(sym)
-for num in numbers:
-    if nr_numbers >= 0:
-        pw_list.append(num)
+for sym in range(0, nr_symbols):
+    pw_list += random.choice(symbols)
+
+for num in range(0, nr_numbers):
+    pw_list += random.choice(numbers)
+
 print(pw_list)
+print(f"Your password is {pw_list}")
 
+#You're a stupid fuck because you needed to look at the code. FUCK YOU. NEVER let Claude show you code.
+# \n You struggled 3 days with no code for what? Just to forget to tell Claude no code.
+# \n Weakling. You got this. Never again.
+
+
+#Hard Level
+import random
+pw = []
+for char in range(0, nr_letters):
+    pw += random.choice(letters)
+
+for sym in range(0, nr_symbols):
+    pw += random.choice(symbols)
+
+for num in range(0, nr_numbers):
+    pw += random.choice(numbers)
+
+#Keep in mind += and append() do the same thing here. You're just adding data to the list here.
+
+for char in range(0, nr_letters):
+    pw.append(random.choice(letters))
+
+for sym in range(0, nr_symbols):
+    pw.append(random.choice(symbols))
+
+for num in range(0, nr_numbers):
+    pw.append(random.choice(numbers))
+
+#Version of printing password using join() function
+print(pw)
+random.shuffle(pw)
+password = "".join(pw)
+print(f"Your password is {password}")
+
+#Version of printing password using string concatenation
+
+print(pw)
+random.shuffle(pw)
+password = ""
+for each in pw:
+    password += each
+print(f"Your password is {password}")
