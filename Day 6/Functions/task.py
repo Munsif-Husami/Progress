efb42ed44f23318e5035541ef4606f76a5a1a29e
Munsif <mh742@njit.edu>
2025-11-17 21:01:22 -0500

    Hurdle code 4 WIP

--- Day 6/Functions/task.py
+++ Day 6/Functions/task.py
@@ -68,21 +68,17 @@ def jump():
     while wall_on_right():
         turn_left()
         move()
-        if right_is_clear():
-            turn_right()
-            move()
-            turn_right()
-            while front_is_clear():
-                move()
-        elif wall_in_front():
-            pause()
-            turn_right()
+    if right_is_clear():
+        turn_right()
+        move()
+        turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
 
 
 while not at_goal():
     if wall_in_front():
         jump()
-        while not right_is_clear():
-            jump()
     else:
         move()
