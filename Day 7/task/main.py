c8c14a82c8cd65eba7dbc4cecbdf1a80454b8fa4
Munsif <mh742@njit.edu>
2026-02-12 19:00:43 -0500

    WIP

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -6,45 +6,39 @@ print(chosen_word)
 
 # TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
 
-placeholder = []
-
-word_size = len(chosen_word)
-
-for abc in chosen_word:
-    placeholder.append('_')
-
-print(" ".join(placeholder))
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)
 
 guess = input("Guess a letter: ").lower()
 
 # TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
 
-display = []
+display = ""
 for harf in chosen_word:
     if harf == guess:
-        display.append(guess)
+        display += guess
     else:
-        display.append("_")
+        display += "_"
 
-print(" ".join(display))
+print(display)
 
 # TODO-1: - Use a while loop to let the user guess again.
 
+guess_again = input("Guess Again: ").lower()
+
+def repeat_loop():
+    print(guess_again)
+
+count = range(word_length)
 while not display == chosen_word:
-    print(input("Guess Again: ").lower())
-    for harf in chosen_word:
-        if harf == guess:
-            display.append(guess)
-        else:
-            print("You've won! Yay!")
+    repeat_loop()
 
 # TODO-2: Change the for loop so that you keep the previous correct letters in display.
 
-for harf in chosen_word:
-    if harf == guess:
-        display += harf
-    else:
-        display += "_"
-        print("You lost a life.")
+for index, harf in enumerate(chosen_word):
+    if display[0] != "_":
 
-print(display)
+
